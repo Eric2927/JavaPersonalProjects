b81b78ac3d65b3a04f471a846d8523ddb5aad02e
EricLi <eric2927.li@gmail.com>
2019-03-12 19:50:01 -0400

    Worked on basic calculator and some stuff with waterfall

--- androidstudio-workspace/SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ androidstudio-workspace/SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View view) {
+        EditText numOneText = (EditText) findViewById(R.id.firstNum);
+        EditText numTwoText = (EditText) findViewById(R.id.secondNum);
+
+        String numOneStr = numOneText.getText().toString();
+        String numTwoStr = numTwoText.getText().toString();
+
+        double numOne = Double.parseDouble(numOneStr);
+        double numTwo = Double.parseDouble(numTwoStr);
+        double answer = numOne + numTwo;
+
+        TextView answerText = (TextView) findViewById(R.id.answerLabel);
+        answerText.setText("Answer: " + answer.toString());
     }
 }
