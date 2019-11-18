eae1be23c9d7783000bf8dd42e8d122902a0fa96
EricLi <eric2927.li@gmail.com>
2019-11-18 16:46:09 -0500

    Practice implementation of Iterator

--- eclipse-workspace/Academic/src/Testers/randomTesting.java
+++ eclipse-workspace/Academic/src/Testers/randomTesting.java
@@ -1,8 +1,38 @@
 package Testers;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class randomTesting {
+	
+	class MyArrayList<T> implements Iterable<T> {
+		private T[] _array;
+		private int _numElements;
+		
+		private class ArrayListIterator<T> implements Iterator<T> {
+			int _index = 0;
+			
+			public boolean hasNext() {
+				return _array[_index + 1] != null;
+			}
+			
+			public T next() {
+				return (T) _array[_index ++];
+			}
+			
+			public void remove () {
+				for (int i = _index; i < _numElements; i ++) {
+					_array[_index - 1] = _array[_index];
+				}
+				_index --;
+				_numElements --;
+			}
+		}
+		
+		public Iterator<T> iterator() {
+			return new ArrayListIterator<T>();
+		}
+	}
 	
 	public static void main(String[] args) {
 		String hi = "1";
