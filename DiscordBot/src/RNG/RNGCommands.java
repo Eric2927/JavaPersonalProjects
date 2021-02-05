de47d264c118ce752451a7f9359b405385df376c
EricLi <eric2927.li@gmail.com>
2021-02-05 18:37:23 -0500

    Update RNGCommands.java

--- DiscordBot/src/RNG/RNGCommands.java
+++ DiscordBot/src/RNG/RNGCommands.java
@@ -26,18 +26,26 @@ public class RNGCommands extends ListenerAdapter {
 		// pick x out of y choices randomly
 		else if (args[0].equalsIgnoreCase(Bot.prefix + "pick")) {
 			try {
-				int numTotal;
+				int totalChoices;
 				switch (args.length) {
 				// If only one extra argument presented, then pick 1 out of y
 				case 2:
-					numTotal = Integer.parseInt(args[1]);
-					int numChosen = RNG.nextInt(numTotal) + 1;
+					totalChoices = Integer.parseInt(args[1]);
+					int numChosen = RNG.nextInt(totalChoices) + 1;
 					event.getChannel().sendMessage(Integer.toString(numChosen)).queue();
 					break;
 				case 3:
-					numTotal = Integer.parseInt(args[2]);
+					totalChoices = Integer.parseInt(args[2]);
 					int numToChoose = Integer.parseInt(args[1]);
-					int[] numsChosen;
+					if (numToChoose <= totalChoices && numToChoose >= 0) {
+						String chosenNumbers = "";
+						for (int i = 0; i < numToChoose; i ++) {
+							chosenNumbers += RNG.nextInt(totalChoices) + 1;
+							chosenNumbers += " ";
+						}
+						event.getChannel().sendMessage(chosenNumbers).queue();
+					}
+					
 					break;
 				default:
 					EmbedBuilder invalidParamNumError = new EmbedBuilder();
